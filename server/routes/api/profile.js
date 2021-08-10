92e987b525faefde627a7941b6076fcc36079acd
Carlos Abril <carlos.abril87@outlook.com>
2021-08-10 16:45:13 -0400

    get all profiles and user routes

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,9 +1,23 @@
 import express from 'express';
-
-import { profileController } from '../../controllers/profileController.js'
+import auth from '../../middleware/auth.js'
+import { check } from 'express-validator';
+import { profileGetOne, profileCreateOne, profileGetAll, profileGetOneUser } from '../../controllers/profileController.js'
 
 const router = express.Router();
 
-router.get('/', profileController)
+router.get('/me', auth, profileGetOne);
+router.post('/', [
+    auth,
+    check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+    check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+],
+    profileCreateOne
+);
+router.get('/', profileGetAll)
+router.get('/user/:user_id', profileGetOneUser)
 
 export default router;
