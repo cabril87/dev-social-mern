f00e50200bed73b513265953dd3d5aea6356a345
Carlos Abril <carlos.abril87@outlook.com>
2021-08-11 12:11:14 -0400

    add and delete comments

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { check } from 'express-validator';
 import auth from '../../middleware/auth.js'
-import { postCreate, postGetOne, postGetAll, postDelete } from '../../controllers/postsController.js'
+import { postCreate, postGetOne, postGetAll, postDelete, postLike, postUnLike, postCommentCreate, postCommentDelete } from '../../controllers/postsController.js'
 
 const router = express.Router();
 
@@ -16,5 +16,16 @@ router.post('/', [auth, [
 router.get('/', auth, postGetAll )
 router.get('/:id', auth, postGetOne )
 router.delete('/:id', auth, postDelete )
+router.put('/like/:id', auth, postLike )
+router.put('/unlike/:id', auth, postUnLike )
+router.post('/comment/:id', [auth, [
+    check('text', 'Text is required')
+        .not()
+        .isEmpty()
+]
+],
+    postCommentCreate
+)
+router.delete('/comment/:id/:comment_id', auth, postCommentDelete)
 
 export default router;
