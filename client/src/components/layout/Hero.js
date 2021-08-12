9ed0edacfb5957bc8e0834e6b9fbc913edb7a95e
Carlos Abril <carlos.abril87@outlook.com>
2021-08-12 11:27:47 -0400

    responsive features section

--- client/src/components/layout/Hero.js
+++ client/src/components/layout/Hero.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import dev from '../../img/dev.png'
+import dev from '../../img/dev2.png'
 
 const Hero = () => {
     return (
-        <section className='relative...'>
-            <div className="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28">
-                <div className="flex flex-1 flex-col items-center lg:items-start lg:m-20 lg:ml-44 lg:mr-32">
+        <section className="relative">
+            <div
+                className="container 
+            flex  
+            flex-col-reverse 
+            lg:flex-row 
+            items-center 
+            lg:gap-12 
+            xlg:mr-8
+            lg:h-screen
+            lg:w-screen
+            ">
+                <div className="flex flex-1 mr-8 flex-col items-center lg:items-start  lg:ml-44 lg:mr-12 lg:ml-96">
                     <h2 className="text-color-blue text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
                         <b>DevSocial</b>
                     </h2>
-                    <p className="text-color-grey text-lg text-center lg:text-left mb-6">A place for developers to connect, learn, and share knowledge.</p>
+                    <p className="text-color-grey text-lg text-center lg:text-left mb-6 w-80">A place for developers to connect, learn, and share knowledge.</p>
                     <div className="flex justify-center flex-wrap gap-6">
                         <button type="button" className="btn bg-color-blue text-white w-36 hover:bg-color-grey">Sign Up</button>
                         <button type="button" className="btn bg-color-red text-white w-36 hover:bg-color-grey">Login</button>
                     </div>
                 </div>
-                <div className="flex justify-center flex-1 mb-1 md:mb-16 lg:mb-0 z-10 ">
-                    <img className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full" src={dev} alt="dev" />
+                <div className="flex justify-center lg:mb-0 z-10 mr-8  lg:w-full">
+                    <img src={dev} alt="dev" />
                 </div>
             </div>
             <div
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -23,11 +23,14 @@ module.exports = {
   container: {
     center: true,
     padding: "1rem",
-    screens: {
-      lg: "1124px",
-      xl: "1124px",
-      "2xl": "1124px",
-    },
+    // screens: {
+    //   xs: { max: '575px' }, // Mobile (iPhone 3 - iPhone XS Max).
+    //   sm: { min: '576px', max: '897px' }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
+    //   md: { min: '898px', max: '1199px' }, // Tablet (matches max: iPad Pro @ 1112px).
+    //   lg: { min: '1200px' }, // Desktop smallest.
+    //   xl: { min: '1159px' }, // Desktop wide.
+    //   xxl: { min: '1359px' } // Desktop widescreen.
+    // },
   },
   variants: {
     extend: {},
